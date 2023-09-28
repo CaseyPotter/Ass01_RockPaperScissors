214133614fe1821f853e165631c0b70d117be452
CaseyPotter <potterc3@mail.uc.edu>
2023-09-28 11:34:27 -0400

    DONE FR

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,57 @@ public class Main {
         String playerOne = "";
         String playerTwo = "";
         String more = "y";
+        String playerOneContinue = "y";
+        String playerTwoContinue = "y";
         Scanner in = new Scanner(System.in);
 
-        while (more.equalsIgnoreCase("y")) {
-            System.out.println("Player One enter, your move: [R P S] ");
-            playerOne = in.next();
+        while (more.equalsIgnoreCase("y"))
+        {
+            while (playerOneContinue.equalsIgnoreCase("y"))
+            {
+                System.out.println("Player One enter, your move: [R P S] ");
+                playerOne = in.next();
+                if(playerOne.equalsIgnoreCase("r"))
+                {
+                    playerOneContinue = "n";
+                }
+                else if (playerOne.equalsIgnoreCase("p"))
+                {
+                    playerOneContinue = "n";
+                }
+                else if (playerOne.equalsIgnoreCase("s"))
+                {
+                    playerOneContinue = "n";
+                }
+                else
+                {
+                    System.out.println("Incorret inputs!");
+                    playerOneContinue = "y";
+                }
+            }
 
-            System.out.println("Player Two, enter your move: [R P S] ");
-            playerTwo = in.next();
-
+           while (playerTwoContinue.equalsIgnoreCase("y"))
+           {
+               System.out.println("Player Two, enter your move: [R P S] ");
+               playerTwo = in.next();
+               if(playerTwo.equalsIgnoreCase("r"))
+               {
+                   playerTwoContinue = "n";
+               }
+               else if (playerTwo.equalsIgnoreCase("p"))
+               {
+                   playerTwoContinue = "n";
+               }
+               else if (playerTwo.equalsIgnoreCase("s"))
+               {
+                   playerTwoContinue = "n";
+               }
+               else
+               {
+                   System.out.println("Incorret inputs!");
+                   playerTwoContinue = "y";
+               }
+           }
             if (playerOne.equalsIgnoreCase("R")) {
                 if (playerTwo.equalsIgnoreCase("P")) {
                     System.out.println("Paper covers rock, Player Two wins! ");
@@ -41,7 +83,7 @@ public class Main {
                     System.out.println("Scissors cuts paper! Player One wins! ");
                 }
                 if (playerTwo.equalsIgnoreCase("R")) {
-                    System.out.println("Rock breaks scissors! PLayer Two wins! ");
+                    System.out.println("Rock breaks scissors! Player Two wins! ");
                 }
                 if (playerTwo.equalsIgnoreCase("S")) {
                     System.out.println("Tie Game! ");
@@ -54,6 +96,8 @@ public class Main {
             }
             System.out.println("Do you want to continue? [Y / N] ");
             more = in.next();
+            playerOneContinue = "y";
+            playerTwoContinue = "y";
 
         }
     }
